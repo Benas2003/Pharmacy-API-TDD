813d400943c5af6fb2d3ebef84a37e647cec553b
benaskubilius <kubiliusb2003@gmail.com>
2021-08-26 17:49:47 +0300

    add: DatabaseSeeder.php seeds users with different roles

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
 use Spatie\Permission\Models\Role;
 
@@ -17,5 +18,28 @@ class DatabaseSeeder extends Seeder
         $role1 = Role::create(['name' => 'Administrator']);
         $role2 = Role::create(['name' => 'Pharmacist']);
         $role3 = Role::create(['name' => 'Department']);
+
+        $admin = User::create([
+            'name'=>'Admin',
+            'email'=>'admin@icloud.com',
+            'password'=>bcrypt('Admin'),
+        ]);
+
+        $pharmacist = User::create([
+            'name'=>'Jonas Jonaitis',
+            'email'=>'jonas.jonaitis@icloud.com',
+            'password'=>bcrypt('JJKK'),
+        ]);
+
+        $department = User::create([
+            'name'=>'Centrinė reanimacija (91)',
+            'email'=>'cr@icloud.com',
+            'password'=>bcrypt('CR91'),
+        ]);
+
+
+        $admin->assignRole($role1);
+        $pharmacist->assignRole($role2);
+        $department->assignRole($role3);
     }
 }
