da398dd3e79688d555e110dd5274f4134a6ae751
benaskubilius <kubiliusb2003@gmail.com>
2021-08-26 18:01:22 +0300

    refactor: introduced public const and removed faker

--- tests/Feature/Http/Controllers/API/AuthControllerTest.php
+++ tests/Feature/Http/Controllers/API/AuthControllerTest.php
@@ -3,43 +3,40 @@
 namespace Tests\Feature\Http\Controllers\API;
 
 use App\Models\User;
-use Faker\Factory;
 use Symfony\Component\HttpFoundation\Response as ResponseAlias;
 use Tests\TestCase;
 
 class AuthControllerTest extends TestCase
 {
 
+    public const ADMIN_EMAIL = 'Admin@icloud.com';
+    public const ADMIN_PASSWORD = 'Admin';
+    public const WRONG_ADMIN_PASSWORD = 'dmin';
+    public const WRONG_ADMIN_EMAIL = 'dmin@icloud.com';
+
     public function test_user_can_register(): void
     {
-        $faker = Factory::create();
-        $adminPassword = $faker->password;
-
-        $admin = User::factory()->create([
-            'password'=>bcrypt($adminPassword),
-        ]);
-
-        $adminLoginResponse = $this->post(route('login'),[
-            'email'=> $admin->email,
-            'password'=>$adminPassword,
-            'password_confirmation'=>$adminPassword,
+        $adminLoginResponse = $this->post(route('login'), [
+            'email' => 'admin@icloud.com',
+            'password' => self::ADMIN_PASSWORD,
+            'password_confirmation' => self::ADMIN_PASSWORD,
         ]);
         $adminToken = $adminLoginResponse->json('token');
 
         $user = User::factory()->make();
 
-        $this->withHeader("Authorization","Bearer $adminToken");
+        $this->withHeader("Authorization", "Bearer $adminToken");
         $response = $this->post(route('register'), [
-            'name'=>$user->name,
-            'email'=>$user->email,
-            'password'=>$user->password,
-            'password_confirmation'=>$user->password,
-            'role'=>'Department'
+            'name' => $user->name,
+            'email' => $user->email,
+            'password' => $user->password,
+            'password_confirmation' => $user->password,
+            'role' => 'Department'
         ])
             ->assertStatus(ResponseAlias::HTTP_CREATED);
 
         $response->assertJsonStructure([
-            'user'=>[
+            'user' => [
                 'created_at',
                 'email',
                 'id',
@@ -49,59 +46,45 @@ class AuthControllerTest extends TestCase
             'message'
         ]);
 
-        $this->withHeader("Authorization","Bearer $adminToken");
-        $this->post(route('logout', ))->assertNoContent()->assertStatus(ResponseAlias::HTTP_NO_CONTENT);
+        $this->withHeader("Authorization", "Bearer $adminToken");
+        $this->post(route('logout',))->assertNoContent()->assertStatus(ResponseAlias::HTTP_NO_CONTENT);
     }
 
     public function test_user_can_login(): void
     {
-        $faker = Factory::create();
-        $password = $faker->password;
+        $response = $this->post(route('login'), [
+            'email' => self::ADMIN_EMAIL,
+            'password' => self::ADMIN_PASSWORD,
+            'password_confirmation' => self::ADMIN_PASSWORD,
+        ])
+            ->assertStatus(ResponseAlias::HTTP_CREATED);
 
-        $user = User::factory()->create([
-            'password'=>bcrypt($password)
+        $response->assertJsonStructure([
+            'user' => [
+                'created_at',
+                'email',
+                'id',
+                'name',
+                'updated_at',
+            ],
+            'token'
         ]);
 
-         $response = $this->post(route('login'),[
-            'email'=>$user->email,
-            'password'=>$password,
-            'password_confirmation'=>$password,
-         ])
-             ->assertStatus(ResponseAlias::HTTP_CREATED);
-
-         $response->assertJsonStructure([
-             'user'=>[
-                 'created_at',
-                 'email',
-                 'id',
-                 'name',
-                 'updated_at',
-             ],
-             'token'
-         ]);
-
         $userToken = $response->json('token');
-        $this->withHeader("Authorization","Bearer $userToken");
-        $this->post(route('logout', ))->assertNoContent()->assertStatus(ResponseAlias::HTTP_NO_CONTENT);
+        $this->withHeader("Authorization", "Bearer $userToken");
+        $this->post(route('logout',))->assertNoContent()->assertStatus(ResponseAlias::HTTP_NO_CONTENT);
     }
 
     public function test_user_can_not_login_with_wrong_email(): void
     {
-        $faker = Factory::create();
-        $password = $faker->password;
-
-        $user = User::factory()->create([
-            'password'=>bcrypt($password)
-        ]);
-
-        $response = $this->post(route('login'),[
-            'email'=>'email@email.com',
-            'password'=>$password,
-            'password_confirmation'=>$password,
+        $this->post(route('login'), [
+            'email' => self::WRONG_ADMIN_EMAIL,
+            'password' => self::ADMIN_PASSWORD,
+            'password_confirmation' => self::ADMIN_PASSWORD,
         ])
             ->assertStatus(ResponseAlias::HTTP_UNAUTHORIZED)
             ->assertExactJson([
-                'message'=>'Bad data'
+                'message' => 'Bad data'
             ]);
     }
 
@@ -109,33 +92,28 @@ class AuthControllerTest extends TestCase
     {
         $user = User::factory()->create();
 
-        $response = $this->post(route('login'),[
-            'email'=>$user->email,
-            'password'=>'BigBen',
-            'password_confirmation'=>'BigBen',
+        $response = $this->post(route('login'), [
+            'email' => self::ADMIN_EMAIL,
+            'password' => self::WRONG_ADMIN_PASSWORD,
+            'password_confirmation' => self::WRONG_ADMIN_PASSWORD,
         ])
             ->assertStatus(ResponseAlias::HTTP_UNAUTHORIZED)
             ->assertExactJson([
-                'message'=>'Bad data'
+                'message' => 'Bad data'
             ]);
     }
 
     public function test_user_can_logout(): void
     {
-        $faker = Factory::create();
-        $adminPassword = $faker->password;
 
-        $admin = User::factory()->create([
-            'password'=>bcrypt($adminPassword),
-        ]);
 
-        $userLoginResponse = $this->post(route('login'),[
-            'email'=> $admin->email,
-            'password'=>$adminPassword,
-            'password_confirmation'=>$adminPassword,
+        $userLoginResponse = $this->post(route('login'), [
+            'email' => self::ADMIN_EMAIL,
+            'password' => self::ADMIN_PASSWORD,
+            'password_confirmation' => self::ADMIN_PASSWORD,
         ]);
         $userToken = $userLoginResponse->json('token');
-        $this->withHeader("Authorization","Bearer $userToken");
-        $this->post(route('logout', ))->assertNoContent()->assertStatus(ResponseAlias::HTTP_NO_CONTENT);
+        $this->withHeader("Authorization", "Bearer $userToken");
+        $this->post(route('logout',))->assertNoContent()->assertStatus(ResponseAlias::HTTP_NO_CONTENT);
     }
 }
