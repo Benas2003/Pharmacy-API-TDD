90dcb51320ad4735f3baac303aa27560b7cec37c
benaskubilius <kubiliusb2003@gmail.com>
2021-08-26 13:13:58 +0300

    feat: consignments migration

---
+++ database/migrations/2021_08_26_095844_create_consignments_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateConsignmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('consignments', function (Blueprint $table) {
+            $table->id('id');
+            $table->integer('department_name');
+            $table->string('status')->default('created');
+            $table->timestamps();
+        });
+
+        Schema::create('consignment_products', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('consignment_id');
+            $table->string('VSSLPR');
+            $table->string('name');
+            $table->integer('amount');
+            $table->timestamps();
+            $table->foreign('consignment_id')->references('id')->on('consignments')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('consignments');
+    }
+}
