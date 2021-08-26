804086875c22c01ec7bfacb0caf7e9ec5dc87bec
benaskubilius <kubiliusb2003@gmail.com>
2021-08-26 13:23:12 +0300

    refactor: DatabaseSeeder.php seeds roles

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +14,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $role1 = Role::create(['name' => 'Administrator']);
+        $role2 = Role::create(['name' => 'Pharmacist']);
+        $role3 = Role::create(['name' => 'Department']);
     }
 }
