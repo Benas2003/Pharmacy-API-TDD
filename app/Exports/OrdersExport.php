5be01054f1b83a2f2ceee3ef87ccdbc4b5e91161
benaskubilius <kubiliusb2003@gmail.com>
2021-08-28 15:50:53 +0300

    feat: collection with selected rows and titles

--- app/Exports/OrdersExport.php
+++ app/Exports/OrdersExport.php
@@ -13,6 +13,17 @@ class OrdersExport implements FromCollection
     */
     public function collection(): Collection
     {
-        return Order::where('status', 'Ordered')->where('created_at','like', date('Y-m-d').'%')->get();
+        $title = [
+            'id'=>'Order ID',
+            'EUR_INT_O'=>'EUR-INT-O Serial No.',
+            'name'=> 'Name',
+            'amount'=>'Amount',
+            'price'=>'Price',
+            'created_at'=>'Order created at'
+        ];
+
+        $orders = Order::select('id', 'EUR_INT_O', 'name', 'amount', 'price', 'created_at')->where('status', 'Ordered')->where('created_at','like', date('Y-m-d').'%')->get();
+        $orders->prepend($title);
+        return $orders;
     }
 }
